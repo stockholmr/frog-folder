499ca3f0fd7e160cbef6de2ceb0be9fe865f4a21
RStockholm <r.stockholm@hotmail.com>
2021-02-04 20:54:33 +0000

    updated

--- widget.js
+++ widget.js
@@ -30,12 +30,19 @@ Com.Frog.Utils.require(
         },
 
         packageID: '2E3636692001B28FC76F4F34DAC60A0730A2A13CF36A730A',
+
+        dataStoreUUID: null,
+        files: [],
+
         /**
          * Constructor. Runs when the widget is first loaded.
          *
          * @method init
          */
-        init: function() {},
+        init: function() {
+            var widget = this;
+            widget.loadFiles(); 
+        },
 
         /**
          * Event fired by the Site Controller.
@@ -54,9 +61,43 @@ Com.Frog.Utils.require(
          * @event 'widget.edit'
          */
         'widget.edit': function(el, ev, data) {
-            this.element.html(
-                this.view('./widget.ejs')
+            var widget = this;
+
+            widget.element.html(
+                widget.view('./widget.ejs')
             );
+
+            var addFileButton = $('<button class="primary">Add File</button>');
+            var toolbar = widget.element.find('#folder-admin');
+            toolbar.append(addFileButton);
+
+            
+            addFileButton.on('click', function(ev) {
+                $('.os_core:first').trigger('os.app.upload', {
+                    "data": {
+                        "upload_type": "asset",
+                        "site_uuid": widget.options.site_uuid,
+                        "focusApp": widget.element.closest('div.app_dialog'),
+                        "callback": function(result) {
+                            if (result.files && result.files.length) {
+                                $.each(result.files, function(i, file) {
+                                    widget.files.push({
+                                        uuid: file.uuid,
+                                        name: file.attachment.name,
+                                        ext: file.attachment.file.mime.ext,
+                                        url:file.external_url,
+                                        dadded:file.attachment.updated
+                                    });
+                                });
+                                widget.saveFiles();
+                            }
+                        }
+                    }
+                });
+            });
+            
+            
+
         },
 
         /**
@@ -64,6 +105,41 @@ Com.Frog.Utils.require(
          *
          * @event 'widget.updated'
          */
-        'widget.updated': function(el, ev, data) {}
+        'widget.updated': function(el, ev, data) {},
+
+
+        loadFiles: function() {
+            var widget = this;
+            FrogOS.fdp({
+                url: 'datastore/get',
+                type: 'GET',
+                path: '/api/fdp/2/',
+                data: {
+                    target_uuid: widget.options.content_uuid,
+                    alias: 'files'
+                }
+            }).done(function(response) {
+                console.log(response);
+                if(response.status === 'ok' && response.response.length) {
+                    widget.files = response.response;
+                }
+            });
+        },
+
+        saveFiles: function() {
+            var widget = this;
+            FrogOS.fdp({
+                url: 'datastore/create',
+                type: 'POST',
+                path: '/api/fdp/2/',
+                data: {
+                    target_uuid: widget.options.content_uuid,
+                    data: widget.files,
+                    alias: 'files',
+                }
+            }).done(function(response) {
+                console.log(response);
+            });
+        },
     });
 });
